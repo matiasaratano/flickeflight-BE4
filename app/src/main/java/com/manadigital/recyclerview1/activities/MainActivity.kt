e9ac6e0fc009bc2c92d4baa52135fd745dbb131f
Ezequiel Venuti <129341756+EzequielVenuti@users.noreply.github.com>
2024-05-31 23:33:34 -0300

    se agrega drawer y su menu drawer

--- app/src/main/java/com/manadigital/recyclerview1/activities/MainActivity.kt
+++ app/src/main/java/com/manadigital/recyclerview1/activities/MainActivity.kt
@@ -6,25 +6,51 @@ import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.manadigital.recyclerview1.R
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNavView : BottomNavigationView
-    private lateinit var navHostFragment : NavHostFragment
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var bottomNavView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+
+        // Configurar Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
         bottomNavView = findViewById(R.id.bottom_bar)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
         NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
-        }
 
+        setupDrawerLayout()
+    }
+
+    private fun setupDrawerLayout() {
+        val navController = navHostFragment.navController
+        navigationView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = navHostFragment.navController
+        return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
+    }
 }
-
-
