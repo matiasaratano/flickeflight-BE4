b7589172b11deb9baf1ce6efcb8111e92af3aae0
Nicolas Fernandez <smandes@gmail.com>
2024-06-03 16:33:58 -0300

    ajuste clase FlightDetails

--- app/src/main/java/com/manadigital/recyclerview1/entities/Flight.kt
+++ app/src/main/java/com/manadigital/recyclerview1/entities/Flight.kt
@@ -17,3 +17,18 @@ data class Airport(
     val id: String,
     val time: String
 )
+
+data class FlightDetails(
+    val departureAirport: Airport,
+    val arrivalAirport: Airport,
+    val duration: Int,
+    val airplane: String,
+    val airline: String,
+    val airlineLogo: String,
+    val travelClass: String,
+    val flightNumber: String,
+    val legroom: String,
+    val extensions: List<String>,
+    val overnight: Boolean = false,
+    val oftenDelayedByOver30Min: Boolean = false
+)
