d671aaa3f95ba54fe277e0c16d7591eada1575b7
Kevin Mercs <kevin.mercs.kam@gmail.com>
2024-06-04 01:09:13 -0300

    agrego los recyclerView de destinos y offers funcionales en el layout Explore

--- app/src/main/java/com/manadigital/recyclerview1/holders/ExploreHolder.kt
+++ app/src/main/java/com/manadigital/recyclerview1/holders/ExploreHolder.kt
@@ -3,20 +3,21 @@ package com.manadigital.recyclerview1.holders
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.manadigital.recyclerview1.entities.Destination
 import com.manadigital.recyclerview1.R
 
 class ExploreHolder(view: View) : RecyclerView.ViewHolder(view) {
-    private val imageView: ImageView = view.findViewById(R.id.imageViewDestination)
+    private val imageView: AppCompatImageView = view.findViewById(R.id.imageViewDestination)
     private val textViewName1: TextView = view.findViewById(R.id.textViewDestination)
     private val textViewName2: TextView = view.findViewById(R.id.textViewDestination2)
-    private val textViewCountry: TextView = view.findViewById(R.id.textViewDestination)
+    private val imageViewId: AppCompatImageView = view.findViewById(R.id.imageView5)
 
     fun bind(destination: Destination) {
         imageView.setImageResource(destination.imageResId)
-        textViewName1.text = destination.name.substring(0, minOf(destination.name.length, 45))
-        textViewName2.text = destination.name.substring(45, minOf(destination.name.length, 94))
-        textViewCountry.text = destination.country.substring(0, minOf(destination.country.length, 45))
+        textViewName1.text = destination.name
+        textViewName2.text = destination.country
+        imageViewId.setImageResource(destination.imageId)
     }
 }
